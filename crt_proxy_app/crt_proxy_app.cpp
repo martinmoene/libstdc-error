c48729bdc506cbc554b9a19fd84fd3b4c3aa4d94
dbjdbj <dbj@dbj.org>
2020-08-19 17:41:51 +0200

    updated

--- crt_proxy_app/crt_proxy_app.cpp
+++ crt_proxy_app/crt_proxy_app.cpp
@@ -1,17 +1,17 @@
 
 #include "crt_proxy_app.h"
 
-int main( const int argc, char * argv[] )
+static inline void test_strlen()
 {
-    namespace cpl =  crt_proxy_lib;
+    namespace cpl = crt_proxy_lib;
 
     const char* input_ = "ABRA\v\n\f\r\t CA DABRA";
 
     auto rezult_ = cpl::strlen(input_);
 
-    static_assert(19 == cpl::strlen("ABRA\v\n\f\r\t CA DABRA").value );
+    static_assert(19 == cpl::strlen("ABRA\v\n\f\r\t CA DABRA").value);
 
-    static_assert(1 == cpl::strlen("").value );
+    static_assert(1 == cpl::strlen("").value);
 
     const char* nullstr{};
 
@@ -23,6 +23,19 @@ int main( const int argc, char * argv[] )
         auto msg = stat;
     }
 
-    return 0;
+}
+
+int main( const int argc, char * argv[] )
+{
+    if (EXIT_SUCCESS == dbj_simple_log_startup( argv[0] ))
+    {
+        auto lfp = current_log_file_path();
+
+        FILE* fp_ = dbj_fhandle_log_file_ptr(NULL);
+        
+        if (fp_) { ::fclose(fp_); fp_ = nullptr; }
+    }
+    else
+        return EXIT_FAILURE;
 }
 
--- crt_proxy_app/crt_proxy_app.h
+++ crt_proxy_app/crt_proxy_app.h
@@ -3,3 +3,6 @@
 // #define _CRT_SECURE_NO_WARNINGS 1
 
 #include "../crt_proxy_lib/crt_proxy_lib.h"
+
+#define DBJ_LOG_TESTING
+#include "dbj--simplelog/log.h"
